d32051e7b893002b7b233d59af150b1fb71f5ce2
Doran342545345 <105158686+Doran342545345@users.noreply.github.com>
2024-02-10 20:32:32 +0100

    Update LoadString.lua

--- LoadString.lua
+++ LoadString.lua
@@ -1,5 +1,5 @@
 if game.GameId == 15197136141 then
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/Doran342545345/Dorans-Test-SCripts/main/Factory_Tycoon'),true))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Doran342545345/Dorans-Test-SCripts/main/Factory_Tycoon.lua'),true))()
 else
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/Doran342545345/Dorans-Test-SCripts/main/Test_Hub'),true))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Doran342545345/Dorans-Test-SCripts/main/Universal.lua'),true))()
 end
