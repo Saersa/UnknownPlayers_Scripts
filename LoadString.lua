0bfc70ce99517d01d3e9f450c09a6482d771dc63
Saersa <142904335+Saersa@users.noreply.github.com>
2024-05-09 08:12:57 +0200

    Update LoadString.lua

--- LoadString.lua
+++ LoadString.lua
@@ -1,5 +1,15 @@
-if game.GameId == 0 then
 
-else
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/Saersa/UnknownPlayers_Scripts/main/Universal.lua'),true))()
+local url = "https://raw.githubusercontent.com/Saersa/UnknownPlayers_Scripts/main/Scripts"
+
+local games = {
+
+	
+	
+}
+
+for i,v in next, games do
+    games[i] = table.concat(v:split(' '), '_')
 end
+
+local name = games[game.PlaceId] or games[game.GameId]
+return loadstring(game:HttpGet(url.. "/"..(name or "Universal")..".lua"))()
